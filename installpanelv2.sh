7d77bc1c67e467d4ef08be6bb2c9b747ffbc04d9
zerodevxc <ffmax34567@gmail.com>
2025-01-12 14:46:34 +0700

    Add files via upload

---
+++ installpanelv2.sh
@@ -0,0 +1,30 @@
+
+
+# Minta input dari pengguna
+read -p "Masukkan domain: " domain
+read -p "Masukkan Email: " email
+read -p "Masukkan Password Login: " password
+read -p "Masukkan Subdomain Panel: " subdomain
+read -p "Masukkan Domainnode: " domainnode
+
+bash <(curl -s https://pterodactyl-installer.se) <<EOF
+0
+zero
+zero
+zero
+zero
+Asia/Jakarta
+$email
+$email
+zero
+adm
+adm
+$password
+$subdomain
+y
+y
+y
+y
+yes
+A
+EOF
